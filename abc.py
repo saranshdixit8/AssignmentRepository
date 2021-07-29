82fb27ab6f8d766cbe2ae4510bee6af6732266ce
Saransh Dixit <saransh.dixit@ibm.com>
2021-07-29 11:49:57 +0530

    added logic for multiply and subtraction

--- abc.py
+++ abc.py
@@ -1,3 +1,5 @@
 print("Hello World! my first pyhton program")
 
 print("added add program too")
+
+print("added multiply program too")
---
+++ xyz.py
@@ -0,0 +1,6 @@
+a = 10
+b = 20
+c = a+b
+d = b-a
+print(c)
+print(d)
